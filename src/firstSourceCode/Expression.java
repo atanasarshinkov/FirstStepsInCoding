eba7388e3558e02d6774a5d1b8412e4180481a69
atanasarshinkov <atanas9705@gmail.com>
2017-04-07 16:58:40 +0300

    First Commit

---
+++ src/firstSourceCode/Expression.java
@@ -0,0 +1,8 @@
+package firstSourceCode;
+
+public class Expression {
+    public static void main(String[] args) {
+        int result = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+        System.out.println(result);
+    }
+}
